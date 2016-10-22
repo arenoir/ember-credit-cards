174e81e484b66b0710c47359b71b11ddc111e248
Dimitri Van de Putte <dimitri@vandeputte.be>
2016-10-22 16:28:15 +0200

    deprecation problem Enumarable#contains -> Enumarble#includes

--- addon/utils/is-whitelist-keypress.js
+++ addon/utils/is-whitelist-keypress.js
@@ -5,5 +5,5 @@ const validKeyCodes = Ember.A([9, 8, 46, 27, 13]);
 export default function isDigitKeypress(e) {
   var keyCode = e.keyCode || e.which;
 
-  return validKeyCodes.contains(keyCode);
+  return validKeyCodes.includes(keyCode);
 }
