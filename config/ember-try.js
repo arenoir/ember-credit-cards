22e9931d89534297f44caed672208eaa5dc220d6
Aaron Renoir <aaron@fieldphone.com>
2016-11-04 20:37:05 -0700

    add lts test scenarios and includes-polyfill

--- config/ember-try.js
+++ config/ember-try.js
@@ -8,13 +8,18 @@ module.exports = {
       }
     },
     {
-      name: 'ember-1.13',
+      name: 'ember-2.4-LTS',
       bower: {
         dependencies: {
-          'ember': '~1.13.0'
-        },
-        resolutions: {
-          'ember': '~1.13.0'
+          'ember': 'lts-2-4'
+        }
+      }
+    }, 
+    {
+      name: 'ember-2.8-LTS',
+      bower: {
+        dependencies: {
+          'ember': 'lts-2-8'
         }
       }
     },
