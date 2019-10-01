3c981409ba4d460b043e7d1215a8aa4ae1c39a08
NullVoxPopuli <LPSego3+dev@gmail.com>
2019-10-01 08:35:17 -0700

    Ran codemods to remove jQuery

--- tests/unit/components/credit-card-form-test.js
+++ tests/unit/components/credit-card-form-test.js
@@ -1,156 +1,148 @@
 import { run } from '@ember/runloop';
-import {
-  moduleForComponent,
-  test
-} from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
-moduleForComponent('credit-card-form', {
-  // specify the other units that are required for this test
-  needs: [
-    'component:input-credit-card-number', 
-    'component:input-credit-card-expiration', 
-    'component:input-credit-card-cvc',
-    'component:input-credit-card-zipcode'
-  ]
-});
+module('credit-card-form', function(hooks) {
+  setupTest(hooks);
 
-test('it renders', function(assert) {
-  assert.expect(2);
+  test('it renders', function(assert) {
+    assert.expect(2);
 
-  // creates the component instance
-  var component = this.subject();
-  assert.equal(component._state, 'preRender');
+    // creates the component instance
+    var component = this.owner.factoryFor('component:credit-card-form').create();
+    assert.equal(component._state, 'preRender');
 
-  // renders the component to the page
-  this.render();
-  assert.equal(component._state, 'inDOM');
-});
-
-// tests for standard label
-
-test('it should format Card Number label correctly with default value when no numberLabel provided', function(assert) {
-  this.render();
-
-  var component = this.subject();
-  
-  run(function() {
-    assert.equal(component.$('.cc-number .control-label').text().trim(), 'Card Number', 'label says Card Number');
-  });
-});
-
-test('it should format Security Code label correctly with default value when no securityCodeLabel provided', function(assert) {
-  this.render();
-
-  var component = this.subject();
-  
-  run(function() {
-    assert.equal(component.$('.cc-cvc .control-label').text().trim(), 'Security Code', 'label says Security Code');
-  });
-});
-
-test('it should format Name on Card label correctly with default value when no nameOnCardLabel provided', function(assert) {
-  this.render();
-
-  var component = this.subject();
-  
-  run(function() {
-    assert.equal(component.$('.cc-name .control-label').text().trim(), 'Name on Card', 'label says Name on Card');
-  });
-});
-
-test('it should format Expiration label correctly with default value when no expirationLabel provided', function(assert) {
-  this.render();
-
-  var component = this.subject();
-  
-  run(function() {
-    assert.equal(component.$('.cc-expiration .control-label').first().text().trim(), 'Expiration', 'label says Expiration');
-  });
-});
-
-test('it should format Zip Code label correctly with default value when no zipCodeLabel provided', function(assert) {
-  this.render();
-
-  var component = this.subject();
-
-  run(function() {
-    component.set('zipcodeRequired', true);
+    // renders the component to the page
+    this.render();
+    assert.equal(component._state, 'inDOM');
   });
 
-  run(function() {
-    assert.equal(component.$('.cc-expiration .control-label').last().text().trim(), 'Zip Code', 'label says Zip Code');
-  });
-});
+  // tests for standard label
 
-// tests for custom label
+  test('it should format Card Number label correctly with default value when no numberLabel provided', function(assert) {
+    this.render();
 
-test('it should format Card Number label correctly with custom numberLabel provided', function(assert) {
-  this.render();
-
-  var component = this.subject();
-
-  run(function() {
-    component.set('numberLabel', 'Kaartnummer');
-  });
-  
-  run(function() {
-    assert.equal(component.$('.cc-number .control-label').text().trim(), 'Kaartnummer', 'label says Kaartnummer');
-  });
-});
-
-test('it should format Security Code label correctly with custom securityCodeLabel provided', function(assert) {
-  this.render();
-
-  var component = this.subject();
-
-  run(function() {
-    component.set('securityCodeLabel', 'Veiligheidscode');
-  });
-  
-  run(function() {
-    assert.equal(component.$('.cc-cvc .control-label').text().trim(), 'Veiligheidscode', 'label says Veiligheidscode');
-  });
-});
-
-test('it should format Name on Card label correctly with custom nameOnCardLabel provided', function(assert) {
-  this.render();
-
-  var component = this.subject();
-
-  run(function() {
-    component.set('nameOnCardLabel', 'Naam op Kaart');
-  });
-  
-  run(function() {
-    assert.equal(component.$('.cc-name .control-label').text().trim(), 'Naam op Kaart', 'label says Naam op Kaart');
-  });
-});
-
-test('it should format Expiration label correctly with custom expirationLabel provided', function(assert) {
-  this.render();
-
-  var component = this.subject();
-
-  run(function() {
-    component.set('expirationLabel', 'Vervalt op');
-  });
-  
-  run(function() {
-    assert.equal(component.$('.cc-expiration .control-label').first().text().trim(), 'Vervalt op', 'label says Vervalt op');
-  });
-});
-
-test('it should format Zip Code label correctly with custom zipCodeLabel provided', function(assert) {
-  this.render();
-
-  var component = this.subject();
-
-  run(function() {
-    component.set('zipCodeLabel', 'Postcode');
-    component.set('zipcodeRequired', true);
+    var component = this.owner.factoryFor('component:credit-card-form').create();
+    
+    run(function() {
+      assert.equal(component.$('.cc-number .control-label').text().trim(), 'Card Number', 'label says Card Number');
+    });
   });
 
-  run(function() {
-    assert.equal(component.$('.cc-expiration .control-label').last().text().trim(), 'Postcode', 'label says Postcode');
+  test('it should format Security Code label correctly with default value when no securityCodeLabel provided', function(assert) {
+    this.render();
+
+    var component = this.owner.factoryFor('component:credit-card-form').create();
+    
+    run(function() {
+      assert.equal(component.$('.cc-cvc .control-label').text().trim(), 'Security Code', 'label says Security Code');
+    });
+  });
+
+  test('it should format Name on Card label correctly with default value when no nameOnCardLabel provided', function(assert) {
+    this.render();
+
+    var component = this.owner.factoryFor('component:credit-card-form').create();
+    
+    run(function() {
+      assert.equal(component.$('.cc-name .control-label').text().trim(), 'Name on Card', 'label says Name on Card');
+    });
+  });
+
+  test('it should format Expiration label correctly with default value when no expirationLabel provided', function(assert) {
+    this.render();
+
+    var component = this.owner.factoryFor('component:credit-card-form').create();
+    
+    run(function() {
+      assert.equal(component.$('.cc-expiration .control-label').first().text().trim(), 'Expiration', 'label says Expiration');
+    });
+  });
+
+  test('it should format Zip Code label correctly with default value when no zipCodeLabel provided', function(assert) {
+    this.render();
+
+    var component = this.owner.factoryFor('component:credit-card-form').create();
+
+    run(function() {
+      component.set('zipcodeRequired', true);
+    });
+
+    run(function() {
+      assert.equal(component.$('.cc-expiration .control-label').last().text().trim(), 'Zip Code', 'label says Zip Code');
+    });
+  });
+
+  // tests for custom label
+
+  test('it should format Card Number label correctly with custom numberLabel provided', function(assert) {
+    this.render();
+
+    var component = this.owner.factoryFor('component:credit-card-form').create();
+
+    run(function() {
+      component.set('numberLabel', 'Kaartnummer');
+    });
+    
+    run(function() {
+      assert.equal(component.$('.cc-number .control-label').text().trim(), 'Kaartnummer', 'label says Kaartnummer');
+    });
+  });
+
+  test('it should format Security Code label correctly with custom securityCodeLabel provided', function(assert) {
+    this.render();
+
+    var component = this.owner.factoryFor('component:credit-card-form').create();
+
+    run(function() {
+      component.set('securityCodeLabel', 'Veiligheidscode');
+    });
+    
+    run(function() {
+      assert.equal(component.$('.cc-cvc .control-label').text().trim(), 'Veiligheidscode', 'label says Veiligheidscode');
+    });
+  });
+
+  test('it should format Name on Card label correctly with custom nameOnCardLabel provided', function(assert) {
+    this.render();
+
+    var component = this.owner.factoryFor('component:credit-card-form').create();
+
+    run(function() {
+      component.set('nameOnCardLabel', 'Naam op Kaart');
+    });
+    
+    run(function() {
+      assert.equal(component.$('.cc-name .control-label').text().trim(), 'Naam op Kaart', 'label says Naam op Kaart');
+    });
+  });
+
+  test('it should format Expiration label correctly with custom expirationLabel provided', function(assert) {
+    this.render();
+
+    var component = this.owner.factoryFor('component:credit-card-form').create();
+
+    run(function() {
+      component.set('expirationLabel', 'Vervalt op');
+    });
+    
+    run(function() {
+      assert.equal(component.$('.cc-expiration .control-label').first().text().trim(), 'Vervalt op', 'label says Vervalt op');
+    });
+  });
+
+  test('it should format Zip Code label correctly with custom zipCodeLabel provided', function(assert) {
+    this.render();
+
+    var component = this.owner.factoryFor('component:credit-card-form').create();
+
+    run(function() {
+      component.set('zipCodeLabel', 'Postcode');
+      component.set('zipcodeRequired', true);
+    });
+
+    run(function() {
+      assert.equal(component.$('.cc-expiration .control-label').last().text().trim(), 'Postcode', 'label says Postcode');
+    });
   });
 });
--- tests/unit/components/input-credit-card-expiration-test.js
+++ tests/unit/components/input-credit-card-expiration-test.js
@@ -1,101 +1,95 @@
 import $ from 'jquery';
 import { run } from '@ember/runloop';
-import {
-  moduleForComponent,
-  test
-} from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
-moduleForComponent('input-credit-card-expiration', {
-  // the component: test module is implicitly running in unit test mode,
-  // which will change to integration test mode by default in an upcoming
-  // version of ember-test-helpers.
-  // Add `unit: true` or a `needs:[]` list to explicitly opt in to unit test mode.
-  unit: true
-});
+module('input-credit-card-expiration', function(hooks) {
+  setupTest(hooks);
 
-test('it renders', function(assert) {
-  assert.expect(2);
-  var component = this.subject();
-  assert.equal(component._state, 'preRender');
+  test('it renders', function(assert) {
+    assert.expect(2);
+    var component = this.owner.factoryFor('component:input-credit-card-expiration').create();
+    assert.equal(component._state, 'preRender');
 
-  this.render();
-  assert.equal(component._state, 'inDOM');
-});
-
-
-test('should format month shorthand correctly', function(assert) {
-  var component = this.subject();
-
-  this.render();
-
-  run(function() {
-    component.set('month', '4');
-  });
-
-  run(function() {
-    assert.equal( component.get('value'), '04 / ');
-  });
-});
-
-
-test('should format forward slash shorthand correctly', function(assert) {
-  var component = this.subject();
-  this.render();
-
-  run(function() {
-    component.set('month', '8');
+    this.render();
+    assert.equal(component._state, 'inDOM');
   });
 
 
-  var e = $.Event('keypress');
-      e.which = 47; // '/'
+  test('should format month shorthand correctly', function(assert) {
+    var component = this.owner.factoryFor('component:input-credit-card-expiration').create();
 
-  run(function() {
+    this.render();
+
+    run(function() {
+      component.set('month', '4');
+    });
+
+    run(function() {
+      assert.equal( component.get('value'), '04 / ');
+    });
+  });
+
+
+  test('should format forward slash shorthand correctly', function(assert) {
+    var component = this.owner.factoryFor('component:input-credit-card-expiration').create();
+    this.render();
+
+    run(function() {
+      component.set('month', '8');
+    });
+
+
+    var e = $.Event('keypress');
+        e.which = 47; // '/'
+
+    run(function() {
+      component.$().trigger(e);
+    });
+
+    run(function() {
+      var value = component.get('value');
+      assert.equal(value, '08 / ');
+    });
+  });
+
+
+  test('should only allow numbers', function(assert) {
+    var component = this.owner.factoryFor('component:input-credit-card-expiration').create();
+    this.render();
+
+    run(function() {
+      component.set('month', '4');
+    });
+
+
+    var e = $.Event('keypress');
+        e.which = 100; // 'd'
+
     component.$().trigger(e);
+
+    run(function() {
+      assert.equal( component.get('value'), '04 / ');
+    });
   });
 
-  run(function() {
-    var value = component.get('value');
-    assert.equal(value, '08 / ');
-  });
-});
+  test('should only allow six numbers', function(assert) {
+    var component = this.owner.factoryFor('component:input-credit-card-expiration').create();
+    this.render();
+
+    run(function() {
+      component.set('month', '4');
+      component.set('year', '2015');
+    });
 
 
-test('should only allow numbers', function(assert) {
-  var component = this.subject();
-  this.render();
+    var e = $.Event('keypress');
+        e.which = 52; // 'd'
 
-  run(function() {
-    component.set('month', '4');
-  });
+    component.$().trigger(e);
 
-
-  var e = $.Event('keypress');
-      e.which = 100; // 'd'
-
-  component.$().trigger(e);
-
-  run(function() {
-    assert.equal( component.get('value'), '04 / ');
-  });
-});
-
-test('should only allow six numbers', function(assert) {
-  var component = this.subject();
-  this.render();
-
-  run(function() {
-    component.set('month', '4');
-    component.set('year', '2015');
-  });
-
-
-  var e = $.Event('keypress');
-      e.which = 52; // 'd'
-
-  component.$().trigger(e);
-
-  run(function() {
-    assert.equal( component.get('value'), '04 / 2015');
+    run(function() {
+      assert.equal( component.get('value'), '04 / 2015');
+    });
   });
 });
