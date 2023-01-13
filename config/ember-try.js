2eed077e901bcc827dccb09425d2c1cbad5238df
Philip Dudley <435890+pdud@users.noreply.github.com>
2023-01-13 14:26:53 -0500

    chore: add github actions

--- config/ember-try.js
+++ config/ember-try.js
@@ -6,42 +6,18 @@ module.exports = async function() {
   return {
     scenarios: [
       {
-        name: 'ember-lts-3.12',
+        name: 'ember-lts-3.24',
         npm: {
           devDependencies: {
-            'ember-source': '~3.12.0'
+            'ember-source': '~3.24.0'
           }
         }
       },
       {
-        name: 'ember-lts-3.16',
+        name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
-            'ember-source': '~3.16.0'
-          }
-        }
-      },
-      {
-        name: 'ember-release',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('release')
-          }
-        }
-      },
-      {
-        name: 'ember-beta',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('beta')
-          }
-        }
-      },
-      {
-        name: 'ember-canary',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('canary')
+            'ember-source': '~3.28.0'
           }
         }
       },
